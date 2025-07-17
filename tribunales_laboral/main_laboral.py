e8c3524ce64bee1dc4a00a6fe1abec8f61b48fbf
Uziel Di Paola <udipaola@worldsys.io>
2025-07-17 15:43:35 -0300

    Update main_laboral.py

--- tribunales_laboral/main_laboral.py
+++ tribunales_laboral/main_laboral.py
@@ -17,7 +17,7 @@ from verificacion_worker_laboral import verificacion_worker
 from utils_laboral import forzar_cierre_navegadores, quedan_procesos_navegador
 
 # Configuración centralizada
-RUTA_SALIDA = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Resultados_Globales')
+RUTA_SALIDA = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Resultados_Globales','Resultados_laboral')
 CHECKPOINT_FILE = os.path.join(RUTA_SALIDA, 'checkpoint_tribunales_laboral.json')
 NORDVPN_PATH = r"C:\Program Files\NordVPN"
 PAISES_NORDVPN = ["Chile", "Argentina", "Bolivia", "Paraguay", "Uruguay", "Peru"]
